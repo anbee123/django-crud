669ca3bf3ff5d319ab97b7418b7f4e3233691e6b
An Bui <anhipchipchip@gmail.com>
2023-02-08 00:15:05 -0500

    done with show request

--- library/views.py
+++ library/views.py
@@ -3,10 +3,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import BookSerializer
 from .models import Book
+from django.shortcuts import get_objetc_or_404
 
 # Create your views here.
 
 # def index(request):
+
 #     books = Book.objects.all()
 #     data = list(books.values())
 #     return JsonResponse(data, safe=False)
@@ -41,17 +43,23 @@ class Books(APIView):
         if book.is_valid():
             book.save()
             return Response(book.data, status=status.HTTP_201_CREATED)
+        else: 
+            return Response(book.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BookDetail(APIView):
-    def get(self, request):
+    def get(self, request, pk):
         #show request
         print(request)
+        book = get_object_or_404(Book, pk=pk)
+        data = BookSerializer(book).data
+        return Response(data)
+
     
-    def put(self, request):
+    def put(self, request, pk):
         #update request
         print(request)
 
-    def delete(self,request):
+    def delete(self,request, pk):
         #delete request
         print(request)
